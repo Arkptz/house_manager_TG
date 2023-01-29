797c9f42f0de6133483171214af7d7150dbf6cf9
Arkptz1 <58850875+Arkptz1@users.noreply.github.com>
2023-01-29 13:16:13 +0400

    Update database_connector.py

--- Utility/database_connector.py
+++ Utility/database_connector.py
@@ -138,7 +138,8 @@ class DbHouse:
         return cols_res
 
     async def update_report(self, user_id: int, name_table: str, tasks: dict) -> None:
-        print(user_id, name_table, tasks, type(tasks))
+        if 'tasks' in tasks:
+            tasks = tasks['tasks']
         date = str(datetime.now().date())
         ans = self.cursor.execute(
             f"SELECT * FROM {name_table} WHERE id={user_id} AND date='{date}'").fetchone()
